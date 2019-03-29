4c930a14b459729cf5f4e4143efd4ead2357cd20
Damitha Gunawardena <damitha.gunawardena+github@gmail.com>
2019-03-29 21:56:29 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+/*MIT License
+
+Copyright (c) [2019] [Damitha Gunawardena]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 var express = require('express');
 var app = express();
 var util = require('util');
@@ -44,7 +67,7 @@ app.get('*/varient/output*m3u8', function (req, res) {
 			res.write("#EXT-X-DISCONTINUITY"+"\n");
 		}
 		for (var j =startSegment; j <= endSegment;j++){
-	                res.write("#EXTINF:8,"+"\n");
+	                res.write("#EXTINF:"+json.segmentLength+","+"\n");
         	        res.write(json.baseDomain+json.urlPrefix+"/"+playlistname+"Frag"+(j+1)+"Num"+j+".ts"+"\n");
 		}
             }
